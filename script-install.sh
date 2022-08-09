eb83405edf63baa4e62ff755000213a8d6f5a2f2
Colin Franceschini <colin.franceschini@gmail.com>
2022-08-09 15:36:53 -0700

    update

--- script-install.sh
+++ script-install.sh
@@ -1,13 +1,21 @@
 GREEN=$'\e[0;32m'
 echo "${GREEN}Running install script${NC}"
 echo ""
+echo "${GREEN}removing node_modules and package-lock files${NC}"
+rm -rf node_modules
+rm -rf ./client/node_modules
+rm -rf ./server/node_modules
+rm package-lock.json
+rm ./client/package-lock.json
+rm ./server/package-lock.json
+echo ""
 echo "${GREEN}Installing concurrently in root directory${NC}"
 npm install concurrently
 echo ""
 echo "${GREEN}Installing modules in client directory${NC}"
 cd client
-npm ci
+npm i
 echo ""
 echo "${GREEN}Installing modules in server directory${NC}"
 cd ../server
-npm ci
+npm i
